a10c29cf04ac039fde299601c355c44f721f1f3a
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-04-05 14:40:55 +0200

    added add female user

--- src/main/resources/database_init.sql
+++ src/main/resources/database_init.sql
@@ -98,6 +98,13 @@ BEGIN
         VALUES (iemail,ifirstname,ilastname,iaddress,ibirthdate,TRUE);
 END //
 DELIMITER ;
+DELIMITER //
+CREATE PROCEDURE AddFemaleUser(IN iemail varchar(254),IN ifirstname varchar(255), IN ilastname varchar(255), IN iaddress varchar(255), IN ibirthdate date)
+BEGIN
+    INSERT user(email, firstname, lastname, address, birthdate, gender)
+        VALUES (iemail,ifirstname,ilastname,iaddress,ibirthdate,FALSE);
+END //
+DELIMITER ;
 
 DELIMITER //
 CREATE PROCEDURE AddUser(IN iemail varchar(254),IN ifirstname varchar(255), IN ilastname varchar(255), IN iaddress varchar(255), IN ibirthdate date, IN igender boolean)
